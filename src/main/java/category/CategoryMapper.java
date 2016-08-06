49357c9e45e283f4cdacb41580f580ac2e51ecaa
ChippZanuff <zanuff32167@mail.ru>
2016-08-06 20:14:17 +0300

    https://github.com/ChippZanuff/budget/pull/4/files#diff-985e389794ca284976bde13280be9d4eR10 #1

--- src/main/java/category/CategoryMapper.java
+++ src/main/java/category/CategoryMapper.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 
 public class CategoryMapper implements RowMapper
 {
-
     public Category mapRow (ResultSet rs, int rowNum) throws SQLException
     {
         return new Category(rs.getInt("id"), rs.getString("name"), rs.getString("created"), rs.getString("updated"));
